157bc3a756ff8a10c2107190a8c17a9f5404a450
hugogor232 <83557013+hugogor232@users.noreply.github.com>
2025-11-29 16:31:26 +0000

    1234

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
 
-const SUPABASE_URL = 'YOUR_SUPABASE_URL'
-const SUPABASE_ANON_KEY = 'YOUR_SUPABASE_ANON_KEY'
+const SUPABASE_URL = 'https://xrxjmbgswtmtptqjhdkf.supabase.co'
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhyeGptYmdzd3RtdHB0cWpoZGtmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ0MTQwMTUsImV4cCI6MjA3OTk5MDAxNX0.iWS9nzgu1PNnHi4YaTH9uHUGxE_PYAoOE0FfnWawTfM'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
